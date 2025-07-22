c67a9c014bf283b5334e0c36963bb61b8e9fc83a
31n5ang <31n5ang@gmail.com>
2025-07-22 21:01:40 +0900

    🎉init: 프로젝트 생성

---
+++ src/main/java/kr/minimalest/api/MinimalestApiV2Application.java
@@ -0,0 +1,13 @@
+package kr.minimalest.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinimalestApiV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinimalestApiV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/kr/minimalest/api/MinimalestApiV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.minimalest.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinimalestApiV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
