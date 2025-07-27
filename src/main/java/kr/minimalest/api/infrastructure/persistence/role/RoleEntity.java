970f341957da14128d4e66bfc7be533de609a331
31n5ang <31n5ang@gmail.com>
2025-07-28 03:12:34 +0900

    🛠️refactor: RoleType 칼럼명 변경

--- src/main/java/kr/minimalest/api/infrastructure/persistence/role/RoleEntity.java
+++ src/main/java/kr/minimalest/api/infrastructure/persistence/role/RoleEntity.java
@@ -20,6 +20,6 @@ public class RoleEntity {
     private Long id;
 
     @Enumerated(EnumType.STRING)
-    @Column(name = "name", nullable = false, unique = true)
+    @Column(name = "role_type", nullable = false, unique = true)
     private RoleType roleType;
 }
