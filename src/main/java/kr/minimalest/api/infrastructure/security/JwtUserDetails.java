5df361ef7c34da65df9b9f9f9a7b20417a76488e
31n5ang <31n5ang@gmail.com>
2025-08-14 02:01:26 +0900

    ✨feat: Jwt 인증 전용 Principal 추가

---
+++ src/main/java/kr/minimalest/api/infrastructure/security/JwtUserDetails.java
@@ -0,0 +1,52 @@
+package kr.minimalest.api.infrastructure.security;
+
+import kr.minimalest.api.domain.user.UserUUID;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Getter
+@RequiredArgsConstructor
+public class JwtUserDetails implements UserDetails {
+
+    private final UserUUID userUUID;
+    private final Collection<? extends GrantedAuthority> authorities;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return userUUID.value();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
