a5889485cbeb29579b55ec1881b1f7c56c575dd1
31n5ang <31n5ang@gmail.com>
2025-08-19 13:59:43 +0900

    🛠️refactor: 생성자 검증 로직 추가

--- src/main/java/kr/minimalest/api/web/controller/dto/SignUpRequest.java
+++ src/main/java/kr/minimalest/api/web/controller/dto/SignUpRequest.java
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import kr.minimalest.api.web.validator.PasswordMatches;
+import org.springframework.util.Assert;
 
 @PasswordMatches
 public record SignUpRequest (
@@ -20,4 +21,14 @@ public record SignUpRequest (
         @NotBlank
         @Size(min = 2)
         String confirmPassword
-) { }
+) {
+        public SignUpRequest {
+                Assert.hasText(email, "email은 공백일 수 없습니다.");
+                Assert.hasText(password, "password는 공백일 수 없습니다.");
+                Assert.hasText(confirmPassword, "confirmPassword는 공백일 수 없습니다.");
+        }
+
+        public static SignUpRequest of(String email, String password, String confirmPassword) {
+                return new SignUpRequest(email, password, confirmPassword);
+        }
+}
