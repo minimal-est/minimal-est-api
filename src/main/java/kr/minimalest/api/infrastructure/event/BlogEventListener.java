1aa193987a7893ebc67daa068809f5536ad0db93
31n5ang <31n5ang@gmail.com>
2025-08-20 20:37:09 +0900

    ✨feat: Blog 이벤트 리스너 추가

---
+++ src/main/java/kr/minimalest/api/infrastructure/event/BlogEventListener.java
@@ -0,0 +1,21 @@
+package kr.minimalest.api.infrastructure.event;
+
+import kr.minimalest.api.domain.blog.event.CreatedBlogEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class BlogEventListener {
+
+    @Async
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    public void onCreatedBlog(CreatedBlogEvent event) {
+        log.info("새로운 블로그 생성: {}, {}", event.getBlogId(), event.getPenName());
+    }
+}
