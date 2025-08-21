965bc286cab80a955ae9c6f97efcc6a2b8a0ff9f
31n5ang <31n5ang@gmail.com>
2025-08-21 14:45:58 +0900

    🧪test: Create Article 단위 테스트

---
+++ src/test/java/kr/minimalest/api/application/article/CreateArticleTest.java
@@ -0,0 +1,88 @@
+package kr.minimalest.api.application.article;
+
+import kr.minimalest.api.application.exception.UserHasNotBlogException;
+import kr.minimalest.api.domain.article.Article;
+import kr.minimalest.api.domain.article.ArticleStatus;
+import kr.minimalest.api.domain.article.event.ArticleCreatedEvent;
+import kr.minimalest.api.domain.article.repository.ArticleRepository;
+import kr.minimalest.api.domain.blog.BlogId;
+import kr.minimalest.api.domain.user.UserId;
+import lombok.Getter;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.context.ApplicationEventPublisher;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class CreateArticleTest {
+
+    @InjectMocks
+    CreateArticle createArticle;
+
+    @Mock
+    ArticleRepository articleRepository;
+
+    @Mock
+    BlogResolver blogResolver;
+
+    @Mock
+    ApplicationEventPublisher eventPublisher;
+
+    @Getter
+    static class ArticleFixture {
+        private final UserId userId = UserId.generate();
+        private final BlogId blogId = BlogId.generate();
+        private final Article article = Article.create(blogId);
+
+        public CreateArticleArgument getCreateArticleArgument() {
+            return CreateArticleArgument.of(userId);
+        }
+    }
+
+    ArticleFixture fixture = new ArticleFixture();
+
+    @Nested
+    @DisplayName("글 생성 시")
+    class HappyCreateArticle {
+
+        @Test
+        @DisplayName("올바른 상태의 글 ID를 반환하고 글 생성 이벤트가 발행된다")
+        public void shouldBeReturnArticleIdAndPublishedEvent() {
+            // given
+            given(blogResolver.getBlogId(fixture.userId)).willReturn(fixture.blogId);
+            given(articleRepository.save(any(Article.class))).willReturn(fixture.article.getId());
+
+            // when
+            CreateArticleResult result = createArticle.exec(fixture.getCreateArticleArgument());
+
+            // then
+            assertThat(result.articleId()).isEqualTo(fixture.article.getId());
+            assertThat(fixture.article.getStatus()).isEqualTo(ArticleStatus.DRAFT);
+            assertThat(fixture.article.getCompletedAt()).isNull();
+            verify(eventPublisher, times(1)).publishEvent(any(ArticleCreatedEvent.class));
+        }
+
+        @Test
+        @DisplayName("사용자가 블로그를 개설하지 않았다면 예외가 발생한다")
+        public void shouldThrowExceptionWhenUserHasNotBlog() {
+            // given
+            given(blogResolver.getBlogId(fixture.userId)).willThrow(UserHasNotBlogException.class);
+
+            // when & then
+            assertThrows(UserHasNotBlogException.class, () -> {
+                createArticle.exec(fixture.getCreateArticleArgument());
+            });
+        }
+    }
+}
