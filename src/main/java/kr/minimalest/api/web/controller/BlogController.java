0501ccb5c2548444a5ba4e28399c9c634e1d1ca9
31n5ang <31n5ang@gmail.com>
2025-11-17 16:51:03 +0900

    🛠️refactor: 불필요한 import 제거

--- src/main/java/kr/minimalest/api/web/controller/BlogController.java
+++ src/main/java/kr/minimalest/api/web/controller/BlogController.java
@@ -21,7 +21,6 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Map;
 import java.util.UUID;
 
 @Slf4j
